18cae9ed20044b3ccbf4a25c8e53ac3c61bf943f
陈劭轩 <609206398@qq.com>
2026-07-17 15:35:38 +0800

    更新版本至2.5.0

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -191,7 +191,7 @@
           <div class="about-info">
             <div class="about-row">
               <span class="about-key">版本</span>
-              <span class="about-value">v2.4.3</span>
+              <span class="about-value">v2.5.0</span>
             </div>
             <div class="about-row">
               <span class="about-key">描述</span>
